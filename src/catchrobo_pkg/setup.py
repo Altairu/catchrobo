f0072b661995749b91693e034170d769bc068ed9
Altairu <106.nogu@gmail.com>
2025-06-12 10:30:08 +0900

    first commit

---
+++ src/catchrobo_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'catchrobo_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='altair',
+    maintainer_email='106.nogu@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'can_node = catchrobo_pkg.can_node:main'
+        ],
+    },
+)
